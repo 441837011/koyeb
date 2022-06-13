efff9a4ff8bf30d3fd4d67c0e59bf23177870906
441837011 <sinwonderful@gmail.com>
2022-06-13 22:51:26 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "64b38bb4-6f1c-4405-ae43-56429d44b188",
                         "alterId": 0
                     }
                 ],
